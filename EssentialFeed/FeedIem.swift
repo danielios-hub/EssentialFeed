e81630d37449171b53ff8f57ff8201c8687177ec
Daniel <1asir.daniel.gallego@gmail.com>
2021-06-01 18:02:42 +0200

    Create initial project

---
+++ EssentialFeed/FeedIem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedIem.swift
+//  EssentialFeed
+//
+//  Created by Daniel Gallego Peralta on 1/6/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
